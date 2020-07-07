d37225f9154a6ad1692272dd9b530dc5f6dbc50c
Youngbeom94 <darania@kookmin.ac.kr>
2020-07-07 11:14:45 +0900

    test

---
+++ SHA-512/SHA-512/main.c
@@ -0,0 +1,18 @@
+/*
+ * SHA-512.c
+ *
+ * Created: 2020-07-07 오전 10:41:36
+ * Author : 김영범
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
